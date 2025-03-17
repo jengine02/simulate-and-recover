626668de4b5e5dcd4a1370f412b00cf9878d5df8
jengine02 <jenjin@gmail.com>
2025-03-17 00:27:02 -0700

    equations

---
+++ test/test-simulate-recover.py
@@ -0,0 +1,14 @@
+
+
+
+
+# Testing Parameters
+a = 0.5      # Boundary separation
+v = 0.5      # Drift rate
+Ter = 0.1    # Non-decision time
+N = 10  # Number of simulated trials
+
+sigma = 1.0  # Noise std dev (typically set to 1.0)
+dt = 0.001   # Time step
+
+
